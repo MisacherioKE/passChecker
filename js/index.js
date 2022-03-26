9f730ef387bbd3285e3eac81258fa1ea740a18a2
unknown <calphonce31@gmail.com>
2022-03-26 10:43:26 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,53 @@
+"use strict";
+// Method 1
+
+// document.getElementById("check").onclick = function(){
+//     alert("checking password...")
+//     var passOne = document.getElementById("passOne").value;
+//     var passTwo = document.getElementById("passTwo").value;
+
+//     if(passOne === passTwo){
+//         document.getElementById("message").innerHTML = "Password Correct!";
+//     }else{
+//         document.getElementById("message").innerHTML = "Incorrect Password!";
+//     }
+// }
+
+// Method 2
+document.getElementById("check").onclick = function(){
+    alert("checking password...")
+    let myPromise = new Promise((resolve, reject) =>{
+        var passOne = document.getElementById("passOne").value;
+        var passTwo = document.getElementById("passTwo").value;
+        
+        if(passOne === passTwo){
+            resolve("Password Correct!");
+        }else{
+            reject("Incorrect Password!");
+        }
+    })
+    myPromise.then((data)=>{
+        document.getElementById("message").innerHTML = "Password Correct!";
+    }).catch((error) =>{
+        document.getElementById("message").innerHTML = "Incorrect Password!";
+    })
+}
+
+
+// Method 3
+// document.getElementById("check").onclick = function(){
+
+//     alert("checking password...");
+
+//     var passOne = document.getElementById("passOne").value;
+//     var passTwo = document.getElementById("passTwo").value;
+    
+//     try {
+//         if(passOne === passTwo) throw message.innerHTML = "passwords match!";
+//         if(passOne != passTwo) throw message.innerHTML = "password mismatch!";
+        
+//     } catch (error) {
+//         document.getElementById("message").innerHTML = error;
+//     }
+// }
+
